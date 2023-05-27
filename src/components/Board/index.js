b4cd2a4fa0d329cb61ed1586eae1510018abb643
Junior Cardoso <junior@icdv.com.br>
2023-05-27 16:40:29 -0300

    Declarando um Vencedor

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -12,7 +12,10 @@ class Board extends React.Component {
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = this.state.xIsNext ? 'X' : '0';
+        if (this.calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             squares: squares,
             xIsNext: !this.state.xIsNext,
@@ -29,8 +32,36 @@ class Board extends React.Component {
         );
     }
 
+    calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
+
     render() {
-        const status = 'Próximo jogador: ' + (this.state.xIsNext ? 'X' : "0");
+
+        const winner = this.calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner' + winner;
+        } else {
+            status = 'Próximo Jogador' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
 
         return (
             <div>
